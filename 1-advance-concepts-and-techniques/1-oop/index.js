b46901328c62f09c619748252959ac25e16ecea6
Waleed Tariq <waleedtariq109@outlook.com>
2025-02-15 22:48:42 +0500

    static method

--- 1-advance-concepts-and-techniques/1-oop/index.js
+++ 1-advance-concepts-and-techniques/1-oop/index.js
@@ -1,1 +1,14 @@
 "use strict";
+
+class Cat {
+  constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
+  }
+
+  static staticMethod() {
+    console.log(this);
+  }
+
+  static species = "Felis catus";
+}
