c23473c309c0401cff791fbda0b6cfd2ba3ec1f7
Waleed Tariq <waleedtariq109@outlook.com>
2025-02-15 17:54:18 +0500

    class basics

--- 1-advance-concepts-and-techniques/1-oop/index.js
+++ 1-advance-concepts-and-techniques/1-oop/index.js
@@ -1,3 +1,29 @@
 "use strict";
 
-console.log("OOP");
+class Triangle {
+  getArea() {
+    return (this.a * this.b) / 2;
+  }
+  getHyptenuse() {
+    return Math.sqrt(this.a ** 2 + this.b ** 2);
+  }
+}
+
+const triangleOne = new Triangle();
+
+triangleOne.a = 5;
+triangleOne.b = 12;
+
+console.log(triangleOne);
+
+const area = triangleOne.getArea();
+const hyptenues = triangleOne.getHyptenuse();
+
+console.log(area);
+console.log(hyptenues);
+
+const typeOfClass = typeof triangleOne;
+const instanceOfClass = triangleOne instanceof Triangle;
+
+console.log(typeOfClass);
+console.log(instanceOfClass);
