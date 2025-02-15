b20f4523841749b66fbddbaac03e59e1555fa729
Waleed Tariq <waleedtariq109@outlook.com>
2025-02-15 22:43:14 +0500

    setup

--- 1-advance-concepts-and-techniques/1-oop/index.js
+++ 1-advance-concepts-and-techniques/1-oop/index.js
@@ -1,16 +1,1 @@
 "use strict";
-
-class Cat {
-  constructor(name, breed) {
-    this.name = name;
-    this.breed = breed;
-  }
-
-  static species = "Felis catus";
-}
-
-/**
- * Static properties are tied to the class not the class instance
- */
-
-console.log(Cat.species);
