2eb10c7499291f54d9c5576e527085d552744189
Waleed Tariq <waleedtariq109@outlook.com>
2025-02-15 22:42:12 +0500

    static properties

--- 1-advance-concepts-and-techniques/1-oop/index.js
+++ 1-advance-concepts-and-techniques/1-oop/index.js
@@ -1,32 +1,16 @@
 "use strict";
-class Triangle {
-  constructor(sideA, sideB) {
-    this.a = sideA;
-    this.b = sideB;
+
+class Cat {
+  constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
   }
 
-  getArea() {
-    return (this.a * this.b) / 2;
-  }
-  getHyptenuse() {
-    return Math.sqrt(this.a ** 2 + this.b ** 2);
-  }
-
-  describe() {
-    return `I am a triangle with a area of ${this.getArea()}.`;
-  }
+  static species = "Felis catus";
 }
 
-class ColorTriangle extends Triangle {
-  constructor(sideA, sideB, color) {
-    super(sideA, sideB);
-    this.color = color;
-  }
-}
+/**
+ * Static properties are tied to the class not the class instance
+ */
 
-class ColorMoodTriangle extends ColorTriangle {
-  constructor(sideA, sideB, color, mood) {
-    super(sideA, sideB, color);
-    this.mood = mood;
-  }
-}
+console.log(Cat.species);
