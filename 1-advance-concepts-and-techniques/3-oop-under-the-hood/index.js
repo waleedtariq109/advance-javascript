f8a1ce7156250f873bc4e16bb243e58b80d3b8b9
Waleed Tariq <waleedtariq109@outlook.com>
2025-03-07 23:29:57 +0500

    prototype

--- 1-advance-concepts-and-techniques/3-oop-under-the-hood/index.js
+++ 1-advance-concepts-and-techniques/3-oop-under-the-hood/index.js
@@ -1,6 +1,4 @@
 class Person {
-  name;
-  age;
   constructor(name, age) {
     this.name = name;
     this.age = age;
@@ -8,6 +6,26 @@ class Person {
   getName() {
     return this.name;
   }
+  getAge() {
+    return this.age;
+  }
+}
+
+function Person1(name, age) {
+  this.name = name;
+  this.age = age;
 }
 
 const p1 = new Person("Waleed Tariq", 23);
+const p2 = new Person("Umair Asif", 32);
+
+const p3 = new Person1("Zeeshan Siddique", 26);
+const p4 = new Person1("Hamza Amin", 34);
+
+p3.__proto__.getAge = function () {
+  return this.age;
+};
+
+p3.__proto__.getName = function () {
+  return this.name;
+};
