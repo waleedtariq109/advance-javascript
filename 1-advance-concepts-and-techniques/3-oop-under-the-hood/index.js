36e733d021a5a17c2988b7b03452fd7e23af12a1
Waleed Tariq <waleedtariq109@outlook.com>
2025-03-07 22:57:06 +0500

    setup

--- 1-advance-concepts-and-techniques/3-oop-under-the-hood/index.js
+++ 1-advance-concepts-and-techniques/3-oop-under-the-hood/index.js
@@ -11,16 +11,3 @@ class Person {
 }
 
 const p1 = new Person("Waleed Tariq", 23);
-
-/**
- *
- * What new keyword does?
- *
- * The new keyword does 4 things.
- *
- * 1: It creates an empty object
- * 2: Sets the `this` keyword to that empty object
- * 3: Return the object - return `this`
- * 4: Creates a link to the object prototype
- *
- */
