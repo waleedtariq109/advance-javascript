2014a6880633563902c35b65e81949f3729547fd
Waleed Tariq <waleedtariq109@outlook.com>
2025-03-09 04:45:14 +0500

    prototype

--- 1-advance-concepts-and-techniques/3-oop-under-the-hood/index.js
+++ 1-advance-concepts-and-techniques/3-oop-under-the-hood/index.js
@@ -16,16 +16,16 @@ function Person1(name, age) {
   this.age = age;
 }
 
+Person1.prototype.getAge = function () {
+  return this.age;
+};
+
+Person1.prototype.getName = function () {
+  return this.name;
+};
+
 const p1 = new Person("Waleed Tariq", 23);
 const p2 = new Person("Amjad", 32);
 
 const p3 = new Person1("Chacha Ghafoor", 26);
 const p4 = new Person1("Manzoor", 34);
-
-p3.__proto__.getAge = function () {
-  return this.age;
-};
-
-p3.__proto__.getName = function () {
-  return this.name;
-};
